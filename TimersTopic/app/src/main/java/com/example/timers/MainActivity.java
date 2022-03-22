de22708e10bf9adc5f35239a248add31a0004fb5
VinayTShetty <vinaytshetty@gmail.com>
2022-03-22 23:48:13 +0530

    Timer
    Basic Implementation of the Timer.

--- TimersTopic/app/src/main/java/com/example/timers/MainActivity.java
+++ TimersTopic/app/src/main/java/com/example/timers/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.timers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG=MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Thread Name "+Thread.currentThread().getName()+" Thread ID= "+Thread.currentThread().getId());
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d(TAG, "onCreate: Thread Name "+Thread.currentThread().getName()+" Thread ID= "+Thread.currentThread().getId());
+
+            }
+        },5000);
     }
 }
